c90995aa9a48d8849bc2b409a02b09dbdb73d2fb
Eduardo Ramirez <eduardoramirez@Eduardos-MacBook-Pro.local>
2023-08-19 01:49:30 -0400

    ER - test commit.

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'MSPB' });
 });
 
+router.get('/provider', function(req, res) {
+  res.render('provider', '');
+})
+
+router.get('/manager', function(req, res) {
+  res.render('manager', '');
+})
+
+router.get('/staff', function(req, res) {
+  res.render('staff', '');
+})
+
 router.get('/faqs', function(req, res) {
   res.render('faqs', '');
 })
