040784fb685e714c01c7973a2c24f115580fdb13
Eduardo Ramirez <eduardoramirez@Eduardos-MacBook-Pro.local>
2023-09-10 15:06:27 -0400

    ER - test commit.

--- public/javascripts/addQuestion.js
+++ public/javascripts/addQuestion.js
@@ -5,11 +5,12 @@ function getUUID() {
 
 function addElement(parentElement, qnaData) {
 	var theID = getUUID();
-	var newElement = '<div class="accordion-item accordionItemStyle">';
-	newElement += '<h2 class="accordion-header" id="heading"' + theID + '">';
+	var newElement = '<h2 class="accordion-header" id="heading"' + theID + '" style="margin:0; padding:0; border:0px">';
+	newElement += '<div class="accordion-item accordionItemStyle" style="justify-content: space-between; border:0px">'
 	newElement += '<button class="accordion-button bAccordProp" type="button" data-bs-toggle="collapse" data-bs-target="#coll' + theID + '" aria-expanded="true" aria-controls="coll' + theID + '">';
-	newElement += 'o ' + qnaData.question + '?';
-	newElement += '</button></h2>';
+	newElement += 'o ' + qnaData.question + '?</button>';
+	newElement += '<i class="fa-solid fa-circle-xmark fa-lg xMarkStyle" data-bs-toggle="modal" data-bs-target="#removeModal">';
+	newElement += '</i></div></h2>';
 	newElement += '<div class="accordion-collapse collapse" id="coll' + theID + '" aria-labelledby="heading' + theID + '" data-bs-parent="#accordionQAFromDB">';
 	newElement += '<div class="accordion-body">';
 	newElement += '<h4>' + qnaData.answer + '</h4>';
@@ -32,7 +33,7 @@ function addQuestion(context, question, answer) {
 			datatype: 'JSON'
 		})
 		.done(function(res) {
-			addElement(document.getElementById('accordionQAFromDB'), qnaData);
+			addElement(document.getElementById('accordionItems'), qnaData);
 		})
 		.fail(function(err){
 			console.log('Error:', err);
