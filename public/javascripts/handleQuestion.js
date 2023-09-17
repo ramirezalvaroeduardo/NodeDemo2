331147ff9dade8a3a9b49045898bf29539334147
Eduardo Ramirez <eduardoramirez@Eduardos-MacBook-Pro.local>
2023-09-16 23:50:07 -0400

    ER - test commit.

--- public/javascripts/handleQuestion.js
+++ public/javascripts/handleQuestion.js
@@ -2,6 +2,7 @@
 var theID = null;
 
 $(document).ready(function() {
+	$('#captureModal').on('show.bs.modal', clearElement);
 	$('#deleteModal').on('show.bs.modal', getElementToHandle);
 	$('#updateModal').on('show.bs.modal', getElementToHandle);
 	$('#exeDeleteQuestion').on('click', delQuestion);
@@ -9,6 +10,11 @@ $(document).ready(function() {
 
 function getUUID() {
 	return (Math.random(Date.now()).toString(36).slice(-6));
+}
+
+function clearElement() {
+	document.getElementById("qnaQuestion").value = ''
+	document.getElementById("qnaAnswer").value = '';
 }
 
 function addElement(parentElement, qnaData) {
@@ -25,34 +31,38 @@ function addElement(parentElement, qnaData) {
 	newElement += '<h4 id="ans' + theID + '">' + qnaData.answer + '</h4>';
 	newElement += '</div></div></div>';
 	parentElement.innerHTML += newElement;
+
 }
 
 function addQuestion(context, question, answer) {
-
-	theID = getUUID();
-	var qnaData = {
-		'uuid_key': theID,
-		'context': context,
-		'question': question,
-		'answer':  answer,
+	if(question == '' || answer == '') {
+		alert("Question and Answer fields must have a content...")
+	} else {
+		theID = getUUID();
+		var qnaData = {
+			'uuid_key': theID,
+			'context': context,
+			'question': question,
+			'answer':  answer,
+		}
+		try{
+			$.ajax({
+				method: 'POST',
+				url: '/addQuestion',
+				data: qnaData,
+				datatype: 'JSON'
+			})
+			.done(function(res) {
+				addElement(document.getElementById('accordionItems'), qnaData);
+			})
+			.fail(function(err){
+				console.log('Error:', err);
+			})
+		} catch ( dExc ) {
+	        console.log( 'sendDMail(.) - E [' + dExc.message + ']' );
+	    }
+	    //alert("Add Question under construction.");
 	}
-	try{
-		$.ajax({
-			method: 'POST',
-			url: '/addQuestion',
-			data: qnaData,
-			datatype: 'JSON'
-		})
-		.done(function(res) {
-			addElement(document.getElementById('accordionItems'), qnaData);
-		})
-		.fail(function(err){
-			console.log('Error:', err);
-		})
-	} catch ( dExc ) {
-        console.log( 'sendDMail(.) - E [' + dExc.message + ']' );
-    }
-    //alert("Add Question under construction.");
 }
 
 function delElement(theElement) {
@@ -61,6 +71,8 @@ function delElement(theElement) {
 
 function getElementToHandle(dEvent) {
 	theID = dEvent.relatedTarget.getAttribute('data-bs-uuidkey');
+	document.getElementById("qnaUpdQuestion").value = document.getElementById("que" + theID).innerText;
+	document.getElementById("qnaUpdAnswer").value = document.getElementById("ans" + theID).innerText;
 }
 
 function delQuestion(dEvent) {
